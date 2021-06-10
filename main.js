9ffeb80edf0b433532bdd39e1fb3eb21b9ca6ca7
Eshanika2011 <75015294+Eshanika2011@users.noreply.github.com>
2021-06-10 17:28:52 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var noseX = 0,
+    noseY = 0,
+    rightWristX = 0,
+    leftWristX = 0,
+    differance = 0
+
 function setup() {
     cam = createCapture(VIDEO);
     cam.size(550, 500);
@@ -14,9 +20,22 @@ function modelLoaded() {
 function gotPose(results) {
     if (results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        differance=floor(leftWristX-rightWristX);
+
     }
 }
 
 function draw() {
     background("lightyellow");
+    fill('lightred');
+    stroke('red');
+    square(noseX,noseY,differance);
+    document.getElementById('sites').innerHTML="Width and height of the square will be "+differance;
+    fill('#FFE5B4');
+    textSize(differance)
+    text("Be Happy!😀",noseX,noseY,differance);
 }
